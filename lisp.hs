62704bd5ccbd321ebb1ee2cee2be42a7334ebe93
Marcin Praski <martinp2p@gmail.com>
2017-11-16 11:49:34 +0000

    Added instances of MonadPlus and Alternative

--- lisp.hs
+++ lisp.hs
@@ -1,50 +1,59 @@
 import Data.Char
+import Data.Monoid
 import Control.Monad
+import Control.Applicative
 
 -- Parser definitions
 newtype Parser a = Parser { parse :: String -> Maybe (a, String) }
 
 instance Functor Parser where
     fmap f p = Parser $ \s -> case parse p s of
-                                Nothing -> Nothing
-                                Just (a, s') -> Just (f a, s')
+                                 Nothing      -> Nothing
+                                 Just (a, s') -> Just (f a, s')
 
 instance Applicative Parser where
-    pure a  = Parser $ \s -> Just (a,s)
+    pure a  = Parser $ \s -> Just (a, s)
     f <*> p = Parser $ \s -> case parse f s of
-                                Nothing -> Nothing
+                                Nothing      -> Nothing
                                 Just (g, s') -> case parse p s' of
-                                                   Nothing -> Nothing
+                                                   Nothing       -> Nothing
                                                    Just (a, s'') -> Just (g a, s'')
 
 instance Monad Parser where
-    return a = Parser $ \s -> Just (a,s)
+    return = pure
     p >>= f  = Parser $ \s -> case parse p s of
-                                Nothing -> Nothing
+                                Nothing      -> Nothing
                                 Just (a, s') -> parse (f a) s'
 
-zero :: Parser a
-zero = Parser $ \s -> Nothing
+instance MonadPlus Parser where
+    mzero       = Parser $ \s -> Nothing
+    a `mplus` b = Parser $ \s -> case parse a s of
+                                    Nothing      -> parse b s
+                                    Just (c, s') -> Just (c, s')
+
+instance Alternative Parser where
+    empty = mzero
+    (<|>) = mplus
 
 consume :: Parser Char
 consume  = Parser $ \s -> case s of
-                            []     -> Nothing
-                            (a:as) -> Just (a, as)
+                             []     -> Nothing
+                             (a:as) -> Just (a, as)
 
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy f = consume >>= (\c -> if f c then return c else zero)
+satisfy f = consume >>= (\c -> if f c then return c else empty)
 
 digit :: Parser Char
-digit = satisfy (\c -> elem c ['0'..'9'])
+digit = satisfy $ flip elem ['0'..'9']
 
 lower :: Parser Char
-lower = satisfy (\c -> elem c ['a'..'z'])
+lower = satisfy $ flip elem ['a'..'z']
 
 upper :: Parser Char
-upper = satisfy (\c -> elem c ['A'..'Z'])
+upper = satisfy $ flip elem ['A'..'Z']
 
 whitespace :: Parser Char
-whitespace = satisfy (\c -> elem c [' ', '\n', '\t'])
+whitespace = satisfy $ flip elem [' ', '\n', '\t']
 
 -- Lisp object definitions
 data Object =
